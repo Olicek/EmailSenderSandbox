fb949a0c069abcc928e17a6303d0d4a4f21648be
Petr Olišar <olisar@sportisimo.cz>
2017-07-22 19:35:24 +0200

    Set emails sending and sended, count attempts and remove emails after sends from unsent_emails.
    Signed-off-by: Petr Olišar <olisar@sportisimo.cz>

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $receiver = new \Oli\EmailSender\Persistence\Entities\Person('Petr Olišar', 'petr.olisar@gmail.com');
 $sender = new \Oli\EmailSender\Persistence\Entities\Person('František Vomáčka', 'vomacka@email.cz');
 
-$email = new \Oli\EmailSender\Persistence\Entities\Email($sender, $receiver, 'Ahoj Františku, jak se máš?', 'Testovací e-mail');
+$email = new \Oli\EmailSender\Persistence\Entities\Email($sender, $receiver, 'Ahoj Petře, jak se máš?', 'Testovací e-mail');
 
 $loader = new Nette\DI\ContainerLoader(__DIR__ . '/../temp');
 $class = $loader->load(function(\Nette\DI\Compiler $compiler) {
